bcc333948ee0e274010623d302ad1858e77f1760
FLORENCE BUCHELET <florence.buchelet@gmail.com>
2024-08-05 15:01:21 +0200

    architecture gitignore global and category resolvers creation

--- README.md
+++ README.md
@@ -12,8 +12,13 @@
 
 - Docker & Docker compose
 
+
+## Next.js
+
+This [Next.js](https://nextjs.org/) project was bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
 ## SQLite
 
 SQLite is an integrated simple database.
 
-**Note**: to navigate your SQLite database, you might want to use an IDE extension and therefore might need to install SQLite3 globally (`sudo apt install sqlite3` on Linux, via Homebrew on Mac and via the official exe for Windows)
+**Note**: to navigate your SQLite database, you might want to use an IDE extension and therefore might need to install SQLite3 globally (`sudo apt install sqlite3` on Linux, via Homebrew on Mac and via the official `.exe` for Windows)
---
+++ architecture.lua
@@ -0,0 +1,44 @@
+--[[ lua is only used here for aesthetic ]]
+
+/root
+├── backend
+│   ├── database
+│   │   └── database.sqlite
+│   ├── node_modules
+│   ├── src
+│   │   ├── dataSource
+│   │   │   ├── dataSource.ts
+│   │   │   └── initTestData.ts
+│   │   ├── entities
+│   │   │   ├── Category.ts
+│   │   │   └── Entity.ts
+│   │   ├── resolvers
+│   │   │   ├── CategoryResolvers.ts
+│   │   │   └── EntityResolvers.ts
+│   │   └── index.ts
+│   ├── Dockerfile
+│   ├── package-lock.json
+│   ├── package.json
+│   └── tsconfig.json
+├── frontend
+│   ├── .next
+│   ├── node_modules
+│   ├── public
+│   ├── src
+│   │   ├── app
+│   │   │   ├── globals.css
+│   │   │   ├── layout.tsx
+│   │   │   ├── page.module.css
+│   │   │   └── page.tsx
+│   ├── .eslintrc.json
+│   ├── .gitignore
+│   ├── Dockerfile
+│   ├── next-env.d.ts
+│   ├── next.config.mjs
+│   ├── package-lock.json
+│   ├── package.json
+│   └── tsconfig.json
+├── .gitignore
+├── architecture.lua
+├── docker-compose.yml
+└── README.md
