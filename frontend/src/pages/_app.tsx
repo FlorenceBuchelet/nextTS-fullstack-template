d87be887e432dd0c115f9936f50e5dbfaafeee74
FLORENCE BUCHELET <florence.buchelet@gmail.com>
2024-08-09 16:26:44 +0200

    working on architecture

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import Layout from "@/layouts/Layout";
+import "@/styles/_variables.scss";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const apolloClient = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
+});
+
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        <ApolloProvider client={apolloClient}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </ApolloProvider>
+    );
+}
+
+// Disabling SSR
+export default dynamic(() => Promise.resolve(App), { ssr: false });
